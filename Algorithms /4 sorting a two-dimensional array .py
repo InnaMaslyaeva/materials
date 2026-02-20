9996a1689005b934d2117b7cf760b397e1685da3
InnaMalyaeva <imaslyaeva@gmail.com>
2026-02-20 23:18:11 +0300

    sorting

--- Algorithms /4 sorting a two-dimensional array .py
+++ Algorithms /4 sorting a two-dimensional array .py
@@ -1,20 +1,34 @@
-# сортировка двумерного массива
+# сортировка двумерного массива/словаря
 
-n = 13
+# 1 list
 a = [[16074, 258],[18285, 74],[23738, 58],
      [4801, 65],[2568, 155],[3456, 122],
      [7779, 122],[16074, 255],[5051, 68],
      [21873, 202],[6677, 196],[20649, 283],
      [15669, 112]]
-print(f'origin data: {a=}')
-a1 = sorted(a, reverse = True)
-print(f'sorted data: {a1=}')
+print(f'list origin data: {a=}\n')
 
-for i in range(n):
-    for j in range(i+1, n):
-        print(i, j, a[i][0], a[i][1], a[j][0], a[j][1])
-        delta = abs(a[i][1]-a[j][1])
-        if a[i][0] == a[j][0] and delta ==3:
-            print(i, j)
-            print('    *****', a[i][0], a[i][1], a[j][0], a[j][1]) 
-print(f'sorted data: {a=}')
+a1 = sorted(a, reverse = True)
+print(f'sorted data by a[0] \|/: {a1=}\n')
+
+a2 = sorted(a, key = lambda x: x[1])
+print(f'sorted data by a[1] /|\: {a2=}\n')
+
+# 2 dictionary
+t = {'D': 1467, 'N': 1493, 'K': 1397, 'I': 1469, \
+     'Y': 1447, 'R': 1492, 'W': 1532, 'H': 1419, \
+     'L': 1536, 'T': 1450, 'M': 1457, 'E': 1518, \
+     'P': 1442, 'A': 1400, 'Z': 1451, 'V': 1509, \
+     'F': 1461, 'C': 1501, 'U': 1455, 'J': 1457, \
+     'B': 1471, 'G': 1531, 'S': 1451, 'X': 1442, \
+     'Q': 1478, 'O': 1536}
+print(f'dictionary origin data: {t=}\n')
+
+# сортировка по key
+t1 = sorted(t.items())
+print(f'sorted by values: {t1=}\n')
+
+# сортировка по value
+t2 = sorted(t.items(), key = lambda item : item[1])
+print(f'sorted by keys: {t2=}')
+
